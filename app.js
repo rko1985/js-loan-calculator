2d1b09382469e96eb1cd7af8847d03496e9c1ef1
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-01-17 11:43:25 -1000

    improvement to UI

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 //Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    //hide results
+    document.getElementById('results').style.display = 'none';
+
+    //show loader
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault();
+    
+});
 
 //Calculate Results
-function calculateResults(e){
+function calculateResults(){
     console.log('Calculating...');
 
     //UI vars
@@ -25,15 +36,24 @@ function calculateResults(e){
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+        //show results
+        document.getElementById('results').style.display = 'block';
+        //hide loader
+        document.getElementById('loading').style.display = 'none';
     }else{
         showError('Please check your numbers');
     }
 
-    e.preventDefault();
 }
 
 //Show Error
 function showError(error){
+
+    //hide results
+    document.getElementById('results').style.display = 'none';
+    //hide loader
+    document.getElementById('loading').style.display = 'none';
     //create a div
     const errorDiv = document.createElement('div');
 
